0adf3959aae617b848b514eeec452a8bd59f0ac0
king-Alex-d-great <ogubuikealex@gmail.com>
2023-09-19 14:48:00 +0100

    Feat: Add constants for status codes and numbers

--- src/utils/constants.utils.ts
+++ src/utils/constants.utils.ts
@@ -9,3 +9,22 @@ export const corsOptions: cors.CorsOptions = {
 export enum Roles { "System", "User" };
 export enum ContentType { "Text", "Image" }
 export enum Status { "Sent", "Received", "Pending" }
+
+//Status Codes
+export const BAD_REQUEST = 400;
+export const OK = 200;
+export const CREATED = 201;
+export const NOT_FOUND = 404;
+export const UNAUTHORIZED = 401;
+export const INTERNAL_SERVER_ERROR = 500;
+export const SERVICE_UNAVAILABLE = 503;
+export const PERMANENT_REDIRECT = 301;
+export const TEMPORARY_REDIRECT = 301;
+
+//Numbers
+export const ONE = 1;
+export const MINUSONE = -1;
+export const TWO = 2;
+export const ZERO = 0;
+export const HUNDRED = 100;
+export const BEARER = "bearer";

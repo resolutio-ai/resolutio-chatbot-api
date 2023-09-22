d4e03b8c6763a2ea72afaf401d6be0f422230a4e
king-Alex-d-great <ogubuikealex@gmail.com>
2023-09-22 16:47:51 +0100

    Chore: Validate environment variables

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,10 +1,25 @@
-const path = require("path");
-const dotenv = require("dotenv");
+import path from "path";
+import { config } from 'dotenv'
 
-dotenv.config();
-
-dotenv.config({
+config({
     path: path.resolve(process.cwd(), ".env.test"),
 });
 
-module.exports = process.env;
+// Validate and export the required environment variables
+export const LIGHT_API_KEY = validateEnvVariable('LIGHT_API_KEY');
+export const PRIVATE_KEY = validateEnvVariable('PRIVATE_KEY');
+export const PUBLIC_KEY = validateEnvVariable('PUBLIC_KEY');
+export const PORT = validateEnvVariable('PORT');
+export const GET_APIKEY_ENDPOINT = validateEnvVariable('GET_APIKEY_ENDPOINT');
+export const SIGNED_MESSAGE = validateEnvVariable('SIGNED_MESSAGE');
+export const GATEWAY_BASEURL = validateEnvVariable('GATEWAY_BASEURL');
+
+// Helper function to validate environment variables
+function validateEnvVariable(variableName: string) {
+    const value = process.env[variableName];
+    if (!value) {
+        throw new Error(`Missing value for environment variable: ${variableName}`);
+    }
+    
+    return value;
+}

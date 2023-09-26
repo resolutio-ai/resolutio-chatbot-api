c86cac487e287991872fa8672410694b228994eb
king-Alex-d-great <ogubuikealex@gmail.com>
2023-09-26 21:41:41 +0100

    Chore: Update response

--- src/controller/conversation-controller.ts
+++ src/controller/conversation-controller.ts
@@ -26,27 +26,17 @@ export class ConversationController {
                             "authorRole": Roles.User,
                             "content": {
                                 "contentType": ContentType.Text,
-                                "parts": ["Hello, how are you?"]
+                                "parts": ["Hello, how are you?", "I'm good. How can I assist you today?"]
                             },
                             "status": Status.Sent,
                             "timestamp": "2023-09-11T12:00:00Z"
-                        },
-                        {
-                            "id": "message2",
-                            "authorRole": Roles.System,
-                            "content": {
-                                "contentType": ContentType.Text,
-                                "parts": ["I'm good. How can I assist you today?"]
-                            },
-                            "status": Status.Received,
-                            "timestamp": "2023-09-11T12:10:00Z"
                         },
                         {
                             "id": "message3",
                             "authorRole": Roles.User,
                             "content": {
                                 "content_type": ContentType.Text,
-                                "parts": ["I have a question about my account."]
+                                "parts": ["I have a question about my account.", "What would you like to know?"]
                             },
                             "status": Status.Sent,
                             "timestamp": "2023-09-11T12:10:00Z"
@@ -56,58 +46,13 @@ export class ConversationController {
                             "authorRole": Roles.System,
                             "content": {
                                 "content_type": ContentType.Text,
-                                "parts": ["What would you like to know?"]
+                                "parts": ["What are intellectual property rights?", "Intellectual property rights are the rights given to persons over the creations of their minds."]
                             },
                             "status": Status.Received,
                             "timestamp": "2023-09-11T12:10:00Z"
                         },
                     ]
-                },
-                {
-                    "_id": "conversation2",
-                    "messages": [
-                        {
-                            "id": "message1",
-                            "authorRole": Roles.User,
-                            "content": {
-                                "contentType": ContentType.Text,
-                                "parts": ["What is IP rights?"]
-                            },
-                            "status": Status.Sent,
-                            "timestamp": "2023-09-11T12:00:00Z"
-                        },
-                        {
-                            "id": "message2",
-                            "authorRole": Roles.System,
-                            "content": {
-                                "contentType": ContentType.Text,
-                                "parts": ["Intellectual property rights are the rights given to persons over the creations of their minds."]
-                            },
-                            "status": Status.Received,
-                            "timestamp": "2023-09-11T12:10:00Z"
-                        },
-                        {
-                            "id": "message3",
-                            "authorRole": Roles.User,
-                            "content": {
-                                "content_type": ContentType.Text,
-                                "parts": ["Awesome. What are some of mine?"]
-                            },
-                            "status": Status.Sent,
-                            "timestamp": "2023-09-11T12:10:00Z"
-                        },
-                        {
-                            "id": "message4",
-                            "authorRole": Roles.System,
-                            "content": {
-                                "content_type": ContentType.Text,
-                                "parts": ["Patents, trademarks, copyrights, and trade secrets are valuable assets of the company and understanding how they work and how they are created is critical to knowing how to protect them"]
-                            },
-                            "status": Status.Received,
-                            "timestamp": "2023-09-11T12:10:00Z"
-                        },
-                    ]
-                },
+                }
             ]
         });
 

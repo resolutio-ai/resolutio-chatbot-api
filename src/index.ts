bdb48bd27f10b38a05222b28453098dd7131075a
king-Alex-d-great <ogubuikealex@gmail.com>
2023-11-20 23:50:38 +0100

    Chore: Add ody parser middleware

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import express from "express";
-import cors from "cors"
-import { connectDatabase } from "./config/database.config";
+import cors from "cors";
 import { apiRoutes } from "./routes";
 import { corsOptions } from "./utils/constants.utils";
-import { getApiKey } from "./integrations/lighthouse/apikey";
 import { PORT } from "./config/env.config";
+import bodyParser from "body-parser";
 
 const app = express();
 
 //connectDatabase();
+
+app.use(
+    bodyParser.urlencoded({
+        extended: true
+    }));
 
 app.use(cors(corsOptions));
 

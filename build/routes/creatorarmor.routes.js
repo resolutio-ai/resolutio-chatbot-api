da519741b5b144b23c2a5508e97a4ec8df10e317
king-Alex-d-great <ogubuikealex@gmail.com>
2023-11-20 22:11:28 +0100

    Build: Update

--- build/routes/creatorarmor.routes.js
+++ build/routes/creatorarmor.routes.js
@@ -6,8 +6,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const creatorarmour_controller_1 = __importDefault(require("../controller/creatorarmour.controller"));
 const multer_utils_1 = require("../utils/multer.utils");
+const constants_utils_1 = require("../utils/constants.utils");
 const creatorArmorRouter = express_1.default.Router();
 creatorArmorRouter
-    .post("/", multer_utils_1.upload.array("artWork", 2), creatorarmour_controller_1.default.createTimeStamp)
+    .post("/", multer_utils_1.upload.array("artWork", constants_utils_1.TWO), creatorarmour_controller_1.default.createTimeStamp)
     .get("/", creatorarmour_controller_1.default.getCreatedWork);
 exports.default = creatorArmorRouter;

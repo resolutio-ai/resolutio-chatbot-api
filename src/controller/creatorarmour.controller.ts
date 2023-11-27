ef1d3500073f384fb6f905c9fd38261d03792ec6
king-Alex-d-great <ogubuikealex@gmail.com>
2023-11-27 23:49:03 +0100

    Chore: Update CreateTimeStamp Functionalty

--- src/controller/creatorarmour.controller.ts
+++ src/controller/creatorarmour.controller.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from "express";
-import { BAD_REQUEST, INTERNAL_SERVER_ERROR, OK, ONE } from "../utils/constants.utils";
+import { BAD_REQUEST, DEFAULT_CHAIN, INTERNAL_SERVER_ERROR, OK, ONE } from "../utils/constants.utils";
 import { storeFiles } from "../integrations/web3storage";
 import creatorarmourServices from "../services/creatorarmor.services";
 import { CreatedWork } from "../models/creatorarmor.schema";
+import { uploadArtWorkSchema } from "../utils/validation.utils";
 
 class CreatorArmourController {
     getCreatedWork = async (request: Request, response: Response) => {
@@ -28,14 +29,16 @@ class CreatorArmourController {
 
     createTimeStamp = async (request: Request, response: Response) => {
         const files = request.files as Express.Multer.File[];
-        const { chainName } = request.query;
+        
+        const artworkDetails = await uploadArtWorkSchema.validate(request.body);
+
+        let { chainName } = request.query;
+
+        chainName ??= DEFAULT_CHAIN;
 
         if (!files || files.length < ONE || !chainName) {
             return response.status(BAD_REQUEST).send({ message: "Invalid Parameters Sent" });
         }
-
-        let cid;
-        let hash;
 
         try {
             const filesArray = files.map(multerFile => {
@@ -44,20 +47,23 @@ class CreatorArmourController {
                 return new File([buffer], originalname, { type: mimetype });
             });
 
-            cid = await storeFiles(filesArray);
+            filesArray.concat(new File([JSON.stringify(artworkDetails)], "artDescription", { type: "text/plain;charset=utf-8", lastModified: Date.now() }))
 
-            hash = await creatorarmourServices.getTimeStampHash(cid as string, chainName as string);
+            const cid = await storeFiles(filesArray);
+
+            const hash = await creatorarmourServices.getTimeStampHash(cid as string, chainName as string);
+
+            return response.status(OK).send(
+                {
+                    message: "Success",
+                    Data: { transactionHash: hash }
+                }
+            );
         } catch (error: any) {
-            return response.status(INTERNAL_SERVER_ERROR).send({ message: `An Error Ocurred: \n${error.message}` })
+            return response.status(error?.status ?? INTERNAL_SERVER_ERROR).send({ message: `An Error Ocurred: \n${error.message}` })
         }
-
-        return response.status(OK).send(
-            {
-                message: "Success",
-                Data: { transactionHash: hash }
-            }
-        );
     }
 }
 
 export default new CreatorArmourController();
+

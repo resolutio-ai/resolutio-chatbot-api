d52a62d1bd9d3920cb4b65936991d709476230e9
king-Alex-d-great <ogubuikealex@gmail.com>
2023-12-21 07:04:59 +0100

    Chore: Add Ids to work on smart contract

--- contracts/creatorarmor.sol
+++ contracts/creatorarmor.sol
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.17;
+pragma solidity ^0.8.20;
 
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract CreateArmour is AccessControl {
-
-    bytes32 public constant CREATOR_ROLE = keccak256("CREATOR_ROLE");    
+    bytes32 public constant CREATOR_ROLE = keccak256("CREATOR_ROLE");
 
     error CallerNotCreator(address caller);
 
@@ -13,12 +12,10 @@ contract CreateArmour is AccessControl {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
-    event WorkCreated(    
-        uint128 timeStamp,
-        string cid
-    );
+    event WorkCreated(uint128 timeStamp, string cid);
 
     struct Work {
+        uint id;
         string cid;
         uint timeStamp;
     }
@@ -29,9 +26,13 @@ contract CreateArmour is AccessControl {
         _grantRole(CREATOR_ROLE, creator);
     }
 
-    function createTimestamp(string calldata _cid) external onlyRole(CREATOR_ROLE){
+    function createTimestamp(
+        string calldata _cid
+    ) external onlyRole(CREATOR_ROLE) {
+        uint currentId = works[msg.sender].length;
 
         Work memory work = Work({
+            id: currentId + 1,
             timeStamp: uint128(block.timestamp),
             cid: _cid
         });
@@ -39,7 +40,13 @@ contract CreateArmour is AccessControl {
         works[msg.sender].push(work);
     }
 
-    function getTimeStamps(address owner) external view returns(Work[] memory){
+    function getTimeStamps(
+        address owner
+    ) external view returns (Work[] memory) {
         return works[owner];
+    }
+
+    function isCreator(address _user) external view returns (bool) {
+        return hasRole(CREATOR_ROLE, _user);
     }
 }

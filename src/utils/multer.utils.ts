1cd571033dee11ad9aa5a86127cdcc4dd55ecd12
king-Alex-d-great <ogubuikealex@gmail.com>
2024-01-08 18:02:57 +0100

    Chore: Update

--- src/utils/multer.utils.ts
+++ src/utils/multer.utils.ts
@@ -3,13 +3,13 @@ import { ONE, allowedImageExtensions } from "./constants.utils";
 import { Request } from "express";
 
 const multerStorage = multer.diskStorage({
-    destination: (_, __, callBack) => {
-        callBack(null, "public");
-    },
-    filename: (_, file, callBack) => {
-        const ext = file.mimetype.split("/")[ONE];
-        callBack(null, `files/admin-${file.fieldname}-${Date.now()}.${ext}`);
-    }
+    // destination: (_, __, callBack) => {
+    //     callBack(null, "public");
+    // },
+    // filename: (_, file, callBack) => {
+    //     const ext = file.mimetype.split("/")[ONE];
+    //     callBack(null, `files/admin-${file.fieldname}-${Date.now()}.${ext}`);
+    // }
 });
 
 const multerFilter = (req: Request, file: Express.Multer.File, callBack: Function) => {

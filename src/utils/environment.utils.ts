71abd5f5757cb32a365e251632fc0337d0ad6de7
king-Alex-d-great <ogubuikealex@gmail.com>
2024-01-08 13:06:29 +0100

    Feat: Configure loading upchain details from env variables

--- src/utils/environment.utils.ts
+++ src/utils/environment.utils.ts
@@ -1,1 +1,24 @@
+
+import { Chain } from "viem";
+import { base, baseGoerli, filecoinCalibration, optimismGoerli, polygon, polygonMumbai, scrollTestnet } from "viem/chains";
+import { DEFAULT_CHAIN } from "./constants.utils";
+
 export type Web3Environment = "polygonTestnet" | "scrollTestnet" | "optimismTestnet" | "filecoinTestnet";
+
+type ChainDictionary = {
+    [key in Web3Environment]: Chain
+}
+
+const chain: ChainDictionary = {
+    polygonTestnet: polygonMumbai,
+    scrollTestnet: scrollTestnet,
+    optimismTestnet: optimismGoerli,
+    filecoinTestnet: filecoinCalibration
+}
+
+const getViemChain = () : Chain => {
+    //Todo: load up chain from env    
+    return chain[DEFAULT_CHAIN];
+}
+
+export { getViemChain }

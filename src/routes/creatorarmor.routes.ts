2217ccef835411607891c5d1b459befbeb5cb529
king-Alex-d-great <ogubuikealex@gmail.com>
2024-01-09 14:20:54 +0100

    Chore: Allow user send personal license

--- src/routes/creatorarmor.routes.ts
+++ src/routes/creatorarmor.routes.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
 import creatorarmourController from "../controller/creatorarmour.controller";
 import { upload } from "../utils/multer.utils";
-import { TWO } from "../utils/constants.utils";
+import { ONE, TWO } from "../utils/constants.utils";
 
 export default Router()
-    .post("/", upload.array("artWorks", TWO), creatorarmourController.createTimeStamp)
+    .post("/", upload.fields([{name:"artWorks", maxCount: ONE}, {name:"userPersonalizedLicense", maxCount: ONE}]), creatorarmourController.createTimeStamp)
     .get("/", creatorarmourController.getCreatedWork);

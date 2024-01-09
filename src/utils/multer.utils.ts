33610ba4e56df4d9ff12ecce0d69aead81e6f660
king-Alex-d-great <ogubuikealex@gmail.com>
2024-01-09 11:51:52 +0100

    Chore: Update To Prevent Storing FIle Locally

--- src/utils/multer.utils.ts
+++ src/utils/multer.utils.ts
@@ -2,15 +2,7 @@ import multer from "multer";
 import { ONE, allowedImageExtensions } from "./constants.utils";
 import { Request } from "express";
 
-const multerStorage = multer.diskStorage({
-    // destination: (_, __, callBack) => {
-    //     callBack(null, "public");
-    // },
-    // filename: (_, file, callBack) => {
-    //     const ext = file.mimetype.split("/")[ONE];
-    //     callBack(null, `files/admin-${file.fieldname}-${Date.now()}.${ext}`);
-    // }
-});
+const multerStorage = multer.memoryStorage();
 
 const multerFilter = (req: Request, file: Express.Multer.File, callBack: Function) => {
     if (allowedImageExtensions.includes(file.mimetype.split("/")[ONE])) {

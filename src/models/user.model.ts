83e24bf37c5c77dc0f387cd40f520c75325e3b52
king-Alex-d-great <ogubuikealex@gmail.com>
2024-03-12 15:19:15 +0100

    Fix: Remove Chatbot Object From User Schema

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -9,25 +9,7 @@ export const userSchema = new Schema<IUser>({
         trim: true,
         required: true,
         unique: true
-    },
-    conversations: {
-        type: [{
-            _id: { type: String },
-            messages: [
-                {
-                    id: { type: Schema.Types.ObjectId },
-                    authorRole: { type: String, enum: Roles, required: true },
-                    content: {
-                        contentType: { type: String, enum: ContentType, required: true },
-                        parts: [String],
-                        cid: String
-                    },
-                    status: { type: String, enum: Status, required: true },
-                    timestamp: Date
-                },
-            ]
-        }], required: false
-    }
+    }  
 });
 
 export const User = model<IUser>("User", userSchema);

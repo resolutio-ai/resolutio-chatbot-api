e7d9ff975d30218bc899c59e1279cf3e3922fc20
ashitha-18 <ashi18pulari@gmail.com>
2024-04-09 20:04:58 +0530

    ref: follow coding standards

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -12,16 +12,19 @@ class UserController {
     getAllUsers = async (request: Request, response: Response) => {
         try {
             const users = await userService.getUser();
-            if (users) {
-                return response.status(OK).send(users);
-            }
-            else {
+            if (!users || users.length < 1) {
                 return response.status(NOT_FOUND).send({
-                    message: `User not found`
+                    message: `No User found`
                 });
             }
+
+            return response.status(OK).send({
+                data: users, 
+                message: "Users retrieved successfully"
+            })
+            
         } catch (error: any) {
-            return response.status(NOT_FOUND).send({
+            return response.status(INTERNAL_SERVER_ERROR).send({
                 message: `An Error Ocurred: \n${error.message}`
             });
         }
@@ -30,15 +33,17 @@ class UserController {
     getUserByWalletAddress = async (request: Request, response: Response) => {
         const { walletAddress } = request.params;
         try {
-            const users = await userService.getUserByWalletAddress(walletAddress as string);
-            if (users) {
-                return response.status(OK).send(users);
-            }
-            else {
+            const user = await userService.getUserByWalletAddress(walletAddress as string);
+            if (!user) {
                 return response.status(NOT_FOUND).send({
                     message: `User not found`
                 });
             }
+            return response.status(OK).send({
+                data: user, 
+                message: "User retrieved successfully"
+            });
+        
         } catch (error: any) {
             return response.status(INTERNAL_SERVER_ERROR).send({
                 message: `An Error Ocurred: \n${error.message}`
@@ -59,67 +64,22 @@ class UserController {
 
     updateUser = async (request: Request, response: Response) => {
         try {
-            const userId = request.params.id;
+            const {userId} = request.params;
             const userData: IUser = request.body;
             const user = await userService.updateUserById(userId, userData);
-            if (user) {
-                return response.status(OK).send({ message: `updated successfully`, user });
-            }
-            else {
+            if (!user) {
                 return response.status(NOT_FOUND).send({
                     message: `User not found`
                 });
             }
+            return response.status(OK).send({ 
+                data : user,
+                message: `updated successfully`
+            });
         } catch (error: any) {
             return response.status(INTERNAL_SERVER_ERROR).send({ error: error.message });
         }
     }
-
-    //get work of a specific user by its id
-    getCreatedWork = async (request: Request, response: Response) => {
-        const userId = request.params.id;
-        const creatorId = request.params.creatorid;
-        try {
-            const user = await userService.getWork(userId);
-
-            if (user) {
-                if (user.works.length > 0) {
-                    user.works.forEach(innerArray => {
-                        innerArray.forEach((work: ICreatorWorkMetadata) => {
-                            if (work.creatorId === creatorId) {
-                                return response.status(OK).send(work);
-                            }
-                        });
-                    });
-                }
-                return response.status(NOT_FOUND).send({
-                    message: `Work not found`
-                });
-            }
-            else {
-                return response.status(NOT_FOUND).send({
-                    message: `User not found`
-                });
-            }
-
-        } catch (error: any) {
-            return response.status(INTERNAL_SERVER_ERROR).send({
-                message: `An Error Ocurred: \n${error.message}`
-            });
-        }
-    };
-    getDelete = async (request: Request, response: Response) => {
-        try {
-            await userService.delete();
-            return response.status(OK).send({
-                message: `success`
-            });
-        } catch (error: any) {
-            return response.status(NOT_FOUND).send({
-                message: `An Error Ocurred: \n${error.message}`
-            });
-        }
-    };
 }
 
 export default new UserController();

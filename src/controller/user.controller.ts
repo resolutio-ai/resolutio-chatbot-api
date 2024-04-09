6f238853d89f89318da4a6ff310573714480eaa4
ashitha-18 <ashi18pulari@gmail.com>
2024-04-10 01:43:41 +0530

    fix: add basic validations

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,11 +1,10 @@
 import { Request, Response } from "express";
-import { NOT_FOUND, INTERNAL_SERVER_ERROR, OK, CREATED ,SocialMediaType, BAD_REQUEST } from "../utils/constants.utils";
+import { NOT_FOUND, INTERNAL_SERVER_ERROR, OK, CREATED, SocialMediaType, BAD_REQUEST } from "../utils/constants.utils";
 import userService from "../services/user.service";
 import { ISocialMediaURLS, IUser } from "../models/interfaces.models";
 
 class UserController {
 
-    // get all the users
     getAllUsers = async (request: Request, response: Response) => {
         try {
             const users = await userService.getUser();
@@ -16,10 +15,10 @@ class UserController {
             }
 
             return response.status(OK).send({
-                data: users, 
+                data: users,
                 message: "Users retrieved successfully"
             })
-            
+
         } catch (error: any) {
             return response.status(INTERNAL_SERVER_ERROR).send({
                 message: `An Error Ocurred: \n${error.message}`
@@ -29,6 +28,9 @@ class UserController {
 
     getUserByWalletAddress = async (request: Request, response: Response) => {
         const { walletAddress } = request.params;
+        if(!walletAddress) {
+            return response.status(BAD_REQUEST).send({ message: "Provide wallet address in request" });
+        }
         try {
             const user = await userService.getUserByWalletAddress(walletAddress as string);
             if (!user) {
@@ -37,10 +39,10 @@ class UserController {
                 });
             }
             return response.status(OK).send({
-                data: user, 
+                data: user,
                 message: "User retrieved successfully"
             });
-        
+
         } catch (error: any) {
             return response.status(INTERNAL_SERVER_ERROR).send({
                 message: `An Error Ocurred: \n${error.message}`
@@ -52,33 +54,45 @@ class UserController {
         try {
             const userData: IUser = request.body;
 
-            if (userData.socialMediaURLs && userData.socialMediaURLs.length > 0){
-                let validURL = validateSocialMediaURLs(userData.socialMediaURLs);
-                if (!validURL){
-                    return response.status(BAD_REQUEST).send({ message: "INVALID SOCIAL MEDIA TYPE" });
-                }    
+            const walletAddress = userData.walletAddress;
+            const checkUser = await userService.getUserByWalletAddress(walletAddress as string);
+            if (checkUser){
+                return response.status(BAD_REQUEST).send({ message: "User with given wallet address exists" });
             }
-            
+
+            if (userData.socialMediaURLs && userData.socialMediaURLs.length > 0) {
+                let validURL = validateSocialMediaURLs(userData.socialMediaURLs);
+                if (!validURL) {
+                    return response.status(BAD_REQUEST).send({ message: "INVALID SOCIAL MEDIA TYPE" });
+                }
+            }
+
             const user = await userService.addMainUser(userData);
-            return response.status(CREATED).send({ 
+            return response.status(CREATED).send({
                 data: user,
-                message: "User created successfully" 
+                message: "User created successfully"
             });
         } catch (error: any) {
             return response.status(INTERNAL_SERVER_ERROR).send({ error: error.message });
         }
     }
-     
+
 
     updateUser = async (request: Request, response: Response) => {
         try {
-            const {id} = request.params;
+            const { id } = request.params;
+            if(!id) {
+                return response.status(BAD_REQUEST).send({ message: "Provide user id in request" });
+            }
             const userData: IUser = request.body;
-            if (userData.socialMediaURLs && userData.socialMediaURLs.length > 0){
+            if (userData.walletAddress){
+                return response.status(BAD_REQUEST).send({ message: "wallet address cannot be updated" });
+            }
+            if (userData.socialMediaURLs && userData.socialMediaURLs.length > 0) {
                 let validURL = validateSocialMediaURLs(userData.socialMediaURLs);
-                if (!validURL){
+                if (!validURL) {
                     return response.status(BAD_REQUEST).send({ message: "INVALID SOCIAL MEDIA TYPE" });
-                }    
+                }
             }
             const user = await userService.updateUserById(id, userData);
             if (!user) {
@@ -87,8 +101,8 @@ class UserController {
                 });
             }
 
-            return response.status(OK).send({ 
-                data : user,
+            return response.status(OK).send({
+                data: user,
                 message: `updated successfully`
             });
         } catch (error: any) {
@@ -98,25 +112,25 @@ class UserController {
 }
 
 function validateSocialMediaURLs(socialMediaURLs: ISocialMediaURLS[]): boolean {
-    for (const obj of socialMediaURLs){
+    for (const obj of socialMediaURLs) {
         switch (obj.nameOfSocialMedia) {
             case SocialMediaType[0]:
-                if (!obj.URLvalue.startsWith("https://twitter.com/")){
+                if (!obj.URLvalue.startsWith("https://twitter.com/")) {
                     return false;
                 }
                 break;
             case SocialMediaType[1]:
-                if (!obj.URLvalue.startsWith("https://www.behance.net/")){
+                if (!obj.URLvalue.startsWith("https://www.behance.net/")) {
                     return false;
                 }
                 break;
             case SocialMediaType[2]:
-                if (!obj.URLvalue.startsWith("https://www.instagram.com/")){
+                if (!obj.URLvalue.startsWith("https://www.instagram.com/")) {
                     return false;
                 }
                 break;
         }
-            
+
     }
     return true;
 }
